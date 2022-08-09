8ace9262c24e54ff8d0daa7d3f9563e155aa2dcb
Sanjay <30881+sanjayts@users.noreply.github.com>
2022-08-09 14:47:09 +0100

    Finishing up the chapter on collections

--- src/chap_17.rs
+++ src/chap_17.rs
@@ -1,7 +1,201 @@
+use regex::Regex;
+use std::borrow::Cow;
+use std::fmt::{Display, Formatter, Write};
+use std::rc::Rc;
+use std::str::FromStr;
+
 #[test]
 fn test_string_collect() {
     let s = "Man hat tan";
     let city: String = s.chars().filter(|c| !c.is_whitespace()).collect();
 
     assert_eq!(city, "Manhattan");
+}
+
+#[test]
+fn test_write() {
+    let mut s = String::new();
+
+    write!(s, "Hey, my name is {}", "Rand").unwrap();
+    assert_eq!(s, "Hey, my name is Rand");
+
+    let mut start = "Man ".to_string();
+    let end = " Middle".to_string();
+    start = start + "In The" + &end;
+    assert_eq!(start, "Man In The Middle");
+}
+
+#[test]
+fn test_drain() {
+    let mut name = "Callindor".to_string();
+    name.drain(..4);
+    assert_eq!(name, "indor");
+}
+
+#[test]
+fn test_replace() {
+    let s = "Dungeons and Dragons".to_string();
+    let s = s.replace('D', "G");
+    assert_eq!(s, "Gungeons and Gragons");
+
+    let s = s.replace(|c| c == 'D', "G");
+    assert_eq!(s, "Gungeons and Gragons");
+}
+
+#[test]
+fn test_split() {
+    let line = "name,age,address,salary,";
+
+    let parts = line.split(',').collect::<Vec<_>>();
+    assert_eq!(parts, vec!["name", "age", "address", "salary", ""]);
+
+    let parts = line.split_terminator(',').collect::<Vec<_>>();
+    assert_eq!(parts, vec!["name", "age", "address", "salary"]);
+}
+
+#[test]
+fn test_trim() {
+    let line = "xasydsdf1234";
+    let trimmed = line.trim_matches(char::is_alphabetic);
+    assert_eq!(trimmed, "1234");
+}
+
+#[test]
+fn test_from_string() {
+    assert_eq!(usize::from_str("123"), Ok(123 as usize));
+    assert_eq!(f64::from_str("123"), Ok(123_f64));
+    assert_eq!(
+        f64::from_str("123_a").unwrap_err().to_string(),
+        "invalid float literal"
+    );
+}
+
+#[derive(Default)]
+struct Person {
+    name: String,
+    age: usize,
+}
+
+impl Display for Person {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Person<name={}, age={}>", self.name, self.age)
+    }
+}
+
+#[test]
+fn test_display() {
+    let p = Person {
+        name: "san".to_string(),
+        age: 21,
+    };
+
+    assert_eq!(format!("{}", p), "Person<name=san, age=21>");
+    assert_eq!(p.to_string(), "Person<name=san, age=21>");
+}
+
+#[test]
+fn test_utf8_from() {
+    let bytes = vec![0x11, 0x44, 0x33, 0x22];
+    let result = String::from_utf8(bytes);
+
+    assert_eq!(result.unwrap(), "\u{11}D3\"");
+
+    let bytes = vec![0xff, 0x44, 0x33, 0x22];
+    let result = String::from_utf8(bytes);
+
+    // into_bytes on the error undoes the ownership done by from_utf8 and returns back the original
+    // data to the user.
+    assert_eq!(
+        result.err().unwrap().into_bytes(),
+        vec![0xff, 0x44, 0x33, 0x22]
+    );
+}
+
+#[test]
+fn test_cow() {
+    std::env::set_var("USERNAME", "Rand");
+    assert_eq!(get_user(), "Rand");
+
+    std::env::remove_var("USERNAME");
+    assert_eq!(get_user(), "Mr. Anonymouse");
+}
+
+fn get_user() -> Cow<'static, str> {
+    std::env::var("USERNAME")
+        .map(|v| v.into())
+        .unwrap_or("Mr. Anonymouse".into())
+}
+
+#[test]
+fn test_pointer_repr() {
+    let original = Rc::new("rand".to_string());
+    let cloned = original.clone();
+    let new = Rc::new("rand".to_string());
+
+    let mut s = String::new();
+
+    writeln!(
+        s,
+        "{:<7} -> {:<14} | {:<14} | {:<14}",
+        "Header", "Original", "Cloned", "New"
+    )
+    .unwrap();
+    writeln!(
+        s,
+        "{:<7} -> {:<14} | {:<14} | {:<14}",
+        "Values", original, cloned, new
+    )
+    .unwrap();
+    write!(
+        s,
+        "{:<7} -> {:p} | {:p} | {:p}",
+        "Pointer", original, cloned, new
+    )
+    .unwrap();
+
+    println!("{}", s);
+    assert!(s.starts_with(
+        "Header  -> Original       | Cloned         | New           \
+    \nValues  -> rand           | rand           | rand          \nPointer ->"
+    ));
+}
+
+#[test]
+fn test_keyword_args_format() {
+    let s = format!(
+        "{description:.<20}{quantity:2} @{cost:5.2}",
+        description = "Caramel Latte",
+        quantity = 2,
+        cost = 4.99
+    );
+    assert_eq!(s, "Caramel Latte....... 2 @ 4.99");
+}
+
+#[test]
+fn test_basic_regex() {
+    let semver = Regex::new(r"(\d+)\.(\d+)\.(\d+)(-[[:alphanum:]-.]*)?").unwrap();
+    let version = r#"regex = "1.5.2-alpha""#;
+
+    assert!(semver.is_match(version));
+
+    let captures = semver.captures(version).unwrap();
+    assert_eq!(&captures[0], "1.5.2-alpha");
+    assert_eq!(&captures[1], "1");
+    assert_eq!(&captures[2], "5");
+    assert_eq!(&captures[3], "2");
+    assert_eq!(&captures[4], "-alpha");
+
+    assert_eq!(captures.get(0).unwrap().as_str(), "1.5.2-alpha");
+    assert_eq!(captures.get(100), None);
+    assert_eq!(captures.get(0).unwrap().start(), 9);
+    assert_eq!(captures.get(0).unwrap().end(), 20);
+
+    let text = "We started with 1.0.0-alpha then moved on to \
+    1.0.0 which was our major release. Then we started working towards 1.1.0-beta";
+    let matches = semver
+        .find_iter(text)
+        .inspect(|m| println!("{:?}", m))
+        .map(|m| m.as_str())
+        .collect::<Vec<_>>();
+    assert_eq!(matches, vec!["1.0.0-alpha", "1.0.0", "1.1.0-beta"]);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,5 @@ pub mod chap_13;
 pub mod chap_14;
 pub mod chap_15;
 pub mod chap_16;
-pub mod json_lib;
 pub mod chap_17;
+pub mod json_lib;
